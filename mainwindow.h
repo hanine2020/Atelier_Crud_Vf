893df042e9655255c9a2ba1402ced1746a28e395
hanine2020 <58153685+hanine2020@users.noreply.github.com>
2019-11-25 18:18:33 +0100

    projet2a

---
+++ mainwindow.h
@@ -0,0 +1,34 @@
+#ifndef MAINWINDOW_H
+#define MAINWINDOW_H
+#include "Personnel.h"
+#include <QMainWindow>
+//#include<QSound>
+//#include <QMediaPlayer>
+#include"personneldialog.h"
+namespace Ui {
+class MainWindow;
+}
+
+class MainWindow : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    explicit MainWindow(QWidget *parent = nullptr);
+    ~MainWindow();
+
+private slots:
+    void on_pb_ajouter_clicked();
+
+    void on_pb_supprimer_clicked();
+
+    void on_pushButton_clicked();
+
+private:
+    Ui::MainWindow *ui;
+    Personnel tmppersonnel;
+    personnelDialog *personneldialog;
+  //  QSound *son ;
+};
+
+#endif // MAINWINDOW_H
